392b5242a41051a30841dbd880129bbf203bf131
Sham-Kh <sham.maher.106@gmail.com>
2026-04-29 23:38:08 -0400

    Update streamlit_sleep_app.py

--- streamlit_sleep_app.py
+++ streamlit_sleep_app.py
@@ -851,7 +851,7 @@ hyp_sample = results['hyp_sample']
             help="30 s viewport. Use Plotly controls to zoom.",
         )
         
-        fig = plotly_eeg_window(times, data, sp_df, results['hyp_sample'], win_start, 30.0, ch_name, sf)
+        fig = plotly_eeg_window(times, data, sp_df, ['hyp_sample'], win_start, 30.0, ch_name, sf)
     st.plotly_chart(fig, use_container_width=True)
 
     with tab_outlook:
